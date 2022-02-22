dc4ab49864b46dafcc0bbefe01643b83fc6e9e96
enes366 <71850398+enes366@users.noreply.github.com>
2022-02-23 00:51:22 +0300

    Add files via upload

---
+++ ortalama hesaplama.py
@@ -0,0 +1,26 @@
+print('Bu Sadece meslek lisseliler için hazırlandı')
+
+y1 = input('1. Yazılı : ')
+y2 = input('2. Yazılı : ')
+y3 = input('3. Yazılı : ')
+y4 = input('4. Yazılı : ')
+y5 = input('5. Yazılı : ')
+y6 = input('6. Yazılı : ')
+y7 = input('7. Yazılı : ')
+y8 = input('8. Yazılı : ')
+y9 = input('9. Yazılı : ')
+y10 = input('10. Yazılı : ')
+y11 = input('11. Yazılı : ')
+y12 = input('12. Yazılı : ')
+y13 = input('13. Yazılı : ')
+y14 = input('14. Yazılı : ')
+ortalama=(float(y1)+float(y2)+float(y3)+float(y4)+float(y5)+float(y6)+float(y7)+float(y8)+float(y9)+float(y10)+float(11)+float(12)+float(13)+float(14))/14
+print("Ortalama :{0} ".format(ortalama))
+
+ort = input('Ortalamanızı Girin : ')
+if(int(ort)>=50):
+      print("Geçtiniz")
+else:
+      print("Kaldınız")
+
+input("enes gökçek tarafından hazırlanmıştır")
